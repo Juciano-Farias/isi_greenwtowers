9a0b9c5598d30e02f41dc4eb3cc3f591587f05c0
Juciano Gomes Farias Junior <juciano.junior@lais.huol.ufrn.br>
2023-12-03 13:53:16 -0300

    :wrench: migration: Users table new columns age and floor

---
+++ src/Api.Data/Migrations/20231203164817_AlterTableUsers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTableUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Birth",
+                table: "User",
+                type: "timestamp with time zone",
+                maxLength: 40,
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Floor",
+                table: "User",
+                type: "character varying(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birth",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Floor",
+                table: "User");
+        }
+    }
+}
