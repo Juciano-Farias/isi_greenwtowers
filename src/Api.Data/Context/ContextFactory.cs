032743f057bd241ff7454567913ec2ad9805e03c
Juciano Gomes Farias Junior <juciano.junior@lais.huol.ufrn.br>
2023-12-03 13:40:33 -0300

    :wrench: fix: Database's connection string

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -8,7 +8,7 @@ public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
     public MyContext CreateDbContext(string[] args)
     {
         // Usado para criar Migrations
-        var connectionString = "Host=localhost;Port=5432;Database=pa;Username=postgres;Password=postgres";
+        var connectionString = "Host=localhost;Port=5434;Database=pa;Username=postgres;Password=postgres";
         var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
         optionsBuilder.UseNpgsql(connectionString);
         return new MyContext(optionsBuilder.Options);
